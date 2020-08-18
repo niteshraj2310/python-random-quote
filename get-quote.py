63ac66f2dbc98f0abd77538f3c93151756ffc518
Nitesh_XD <niteshraj231@outlook.com>
2020-08-18 16:14:17 +0530

    Done finally Added new qoutes kek
    Signed-off-by: Nitesh_XD <niteshraj231@outlook.com>

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,9 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = 16
   rnd = random.randint(0, last)
   print(quotes[rnd])
-
 if __name__== "__main__":
   primary()
 
